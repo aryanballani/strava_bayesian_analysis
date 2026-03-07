a2676675a92fcf9ebc214684e8f0e7e1bed35745
elliewake <ellieawake@gmail.com>
2026-03-06 18:01:26 -0800

    First commit

---
+++ data-analysis.R
@@ -0,0 +1,30 @@
+library(dplyr)
+library(ggplot2)
+ellie <- read.csv("ellie.csv") %>%
+  select("date", "elevation_gain", "distance_km", "pace_min_km")
+# Select runs from 2025/26
+ellie <- ellie %>%
+  mutate(date = as.Date(ellie$date)) %>%
+  filter(date > as.Date("2024-01-01"))
+View(ellie)
+# Distance vs pace
+ggplot() +
+  geom_point(data = ellie, aes(x = distance_km, y = pace_min_km)) +
+  labs(x = "Distance (km)", y = "Pace (min/km)") +
+  theme_minimal()
+# Elevation gain vs pace
+ggplot() +
+  geom_point(data = ellie, aes(x = elevation_gain, y = pace_min_km)) +
+  labs(x = "Elevation gain (m)", y = "Pace (min/km)") +
+  theme_minimal()
+# Date vs pace
+ggplot() +
+  geom_point(data = ellie, aes(x = date, y = pace_min_km)) +
+  labs(x = "Date", y = "Pace (min/km)") +
+  theme_minimal()
+# Date vs distance
+ggplot() +
+  geom_point(data = ellie, aes(x = date, y = distance_km)) +
+  labs(x = "Date", y = "Distance (km)") +
+  theme_minimal()
+
